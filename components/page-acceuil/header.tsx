a2342629bb9c7aa069f9af8b2c030d35e242848f
DEKENI Toha <dekenitoha097@gmail.com>
2026-03-02 20:05:43 +0000

    installation motion

--- components/page-acceuil/header.tsx
+++ components/page-acceuil/header.tsx
@@ -2,7 +2,6 @@
 
 export default function Header() {
     return (    
-
         <div className="w-full z-20 bg-red-500 h-full relative flex items-center justify-center">
           <NavBar />
         </div>
