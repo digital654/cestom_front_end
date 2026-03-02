2dbbc535516d6c5808d822d69ac6343dc60df6d8
DEKENI Toha <dekenitoha097@gmail.com>
2026-03-02 18:20:45 +0000

    ajout du composant LastNew dans la page /

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FAQ from "@/components/page-acceuil/faqs";
 import ClientReviews from "@/components/page-acceuil/ClientReviews";
+import LatestNews from "@/components/page-acceuil/latest-news";
 
 export default function Home() {
   return (
     <div className="w-full ">
       <ClientReviews />
       <FAQ />
+      <LatestNews />
     </div>
   );
 }
