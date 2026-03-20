c1d96e3515f35db5a11c106067dbcfb59e635de9
ALOGNON Ayayi Serge <sergealognon2007@gmail.com>
2026-03-20 04:50:08 +0000

    ajout du composant profile-paiement

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { PaymentCard } from "./components/profil/paymentcard";
+import { ProfileCompletion } from "./components/profil/profilecompletion";
+import { DashboardHeader } from "./components/profil/header";
+export default function DashboardPage() {
+  return (
+    
+  );
+}
