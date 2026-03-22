110abfa2d4e6640d6e78dcb6d86e6995e452fd29
DEKENI Toha <dekenitoha097@gmail.com>
2026-03-22 14:14:50 +0100

    ajout des composant header

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,14 @@
 // Imports des composants Documents
+import { DashboardHeader } from "@/components/ui/DashboardHeader";
+import { WelcomeCard } from "@/components/ui/WelcomeCard";
 import DocumentList from "./components/documents/DocumentList";
-  export default function Page() {
+
+export default function Page() {
   return (
-    <main className="min-h-screen bg-gray-50 p-6 flex justify-center">
-      <div className="w-full max-w-3xl">
+    <main >
+        <DashboardHeader />
+        <WelcomeCard /> 
+      <div >
         <DocumentList />
       </div>
     </main>
